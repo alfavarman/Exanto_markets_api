56dffdc1857f1fd07e0234534e583218481ee2b9
CKS <communicate2charles@gmail.com>
2023-07-28 17:42:28 +0700

    ExeclHandler

--- src/excel.py
+++ src/excel.py
@@ -1,103 +1,108 @@
-# list of tickers to skip
-EXCLUDED_TICKERS = ("BTC", "ETH", "USD", "TLT", "BUSD")
-# cell description in execl
-AVAILABLE_CASH = "Dolar amerykański (gotówka) Exante"
-# ticker column
-COL_B = 2
-# exposition column
-COL_E = 5
+import logging
+from typing import List, Dict
+from openpyxl import load_workbook
 
 
-def get_positions_dict_from_sheet(sheet, ticker_column: int) -> dict:
-    """
-    get dictionary key value pairs
-    key: str representing ticker of position
-    value: number representing exel row
-    """
-    # last_row_with_data = sheet.max_row
+class ExelHandler:
+    def __init__(self, file_name: str):
+        self.file_name = file_name
+        self.EXCLUDED_TICKERS = ("BTC", "ETH", "USD", "TLT", "BUSD")
+        self.AVAILABLE_CASH = "Dolar amerykański (gotówka) Exante"
+        self.available_cash_row_col = None
+        # ticker column
+        self.COL_B = 2
+        # exposition column
+        self.COL_E = 5
+        self.sheet = None
+        self.positions = None
 
-    positions = {}
-    for index, row in enumerate(sheet.iter_rows(ticker_column, values_only=True), start=1):
-        # skip excluded tickers
-        if row[1] in EXCLUDED_TICKERS:
-            continue
+    def load_file_data(self) -> None:
+        self._load_available_cash_row()
+        self._load_file_positions()
+        self._load_active_sheet(file_name=self.file_name)
 
-        try:
-            positions[row[1]] = index
-        except KeyError:
-            print(f"Ticker: {row[1]} duplicated in exel sheet")
+    def update_file_with_api_data(self, api_data: dict) -> None:
+        """ update position to file"""
+        api_positions = api_data["positions"]
 
-    return positions
+        for position in api_positions:
+            self._update_position(api_position=position)
 
+        self._update_file_exante_available_cash(api_data["currencies"])
 
-def validate_file_name(name: str, file_types: tuple) -> bool:
-    """ validate file type"""
-    file_type = name.split(".")[-1]
-    return file_type in file_types
+    def save_and_close(self):
+        self.sheet.save()
+        self.sheet.close()
+        logging.info(f"File {self.file_name} updated successfully")
 
-
-def represent_in_thousands(value: float) -> float:
-    """ represent value in thousands"""
-    try:
-        float_value = float(value)
-        result = float_value / 1000
-        return round(result, 1)
-    except ValueError:
-        print(f"ValueError: {value} is not a valid number")
-
-
-def convert_to_float(value: str) -> float:
-    """ convert value to float None converts to 0.0"""
-    if not value:
-        return 0.0
-    try:
-        float_value = float(value)
-        return float_value
-    except ValueError:
-        print(f"ValueError: {value} is not a valid number")
-
-
-def update_file_positions_with_api_data(api_positions: dict, sheet) -> None:
-    """ update position to file"""
-
-    file_positions = get_positions_dict_from_sheet(sheet, ticker_column=COL_B)
-    file_tickers = file_positions.keys()
-
-    for position in api_positions:
-        ticker = position["symbolId"].split(".")[0]
-
-        # confirm ticker exist in file
-        if ticker in file_tickers:
-            position_value = represent_in_thousands(position["convertedValue"])
-            # confirm position is not recently sold
-            cell_value = convert_to_float(sheet.cell(row=file_positions[ticker], column=COL_E).value)
-            if position_value != cell_value:
-                sheet.cell(row=file_positions[ticker], column=COL_E).value = position_value if position_value else None
-                print(f"UPDATE: {ticker} successful")
+    def _load_available_cash_row(self) -> None:
+        for row_number, row in enumerate(self.sheet.iter_rows(values_only=True), start=1):
+            if row[0] == self.AVAILABLE_CASH:
+                self.available_cash_row = row_number
+                break
         else:
-            print(f"ALERT: Ticker {ticker} not found in file")
+            raise Exception(f"{self.AVAILABLE_CASH} not found in file")
 
+    def _load_active_sheet(self, file_name: str) -> None:
+        wb = load_workbook(file_name)
+        self.sheet = wb.active
 
-def update_file_exante_available_cash(available_cash: float, sheet) -> None:
-    """
-    update available cash to file,
-    function update if values are not same,
-    if value is 0.0 cell will be empty
-    """
-    for row_number, row in enumerate(sheet.iter_rows(values_only=True), start=1):
-        if row[0] == AVAILABLE_CASH:
-            cell_value = convert_to_float(sheet.cell(row=row_number, column=COL_E).value)
-            if available_cash != cell_value:
-                sheet.cell(row=row_number, column=COL_E).value = available_cash if available_cash else None
-                print(f"UPDATE: {AVAILABLE_CASH} successful")
-            break
-    else:
-        raise Exception(f"{AVAILABLE_CASH} not found in file")
+    def _load_file_positions(self) -> None:
+        positions = {}
+        for index, row in enumerate(self.sheet.iter_rows(self.COL_B, values_only=True), start=1):
+            # skip excluded tickers
+            if row[1] in self.EXCLUDED_TICKERS:
+                continue
 
+            try:
+                positions[row[1]] = (index, self._repr_in_thousands(row[4]))
+            except KeyError:
+                logging.warning(f"Ticker: {row[1]} duplicated in exel sheet")
 
-def get_currencies_converted_value(data: dict[dict]) -> float: #TODO comprehentions
-    totals = 0.0
-    for currency in data:
-        totals += float(currency["convertedValue"])
-    return represent_in_thousands(totals)
+        self.positions = positions
 
+    def _repr_in_thousands(self, value: str) -> float:
+        """ represent value in thousands"""
+        value = self._convert_to_float(value)
+        try:
+            return round((float(value) / 1000), 1)
+        except ValueError:
+            print(f"ValueError: {value} is not a valid number")
+
+    @staticmethod
+    def _convert_to_float(value: str) -> float:
+        """ convert value to float None converts to 0.0"""
+        if not value:
+            return 0.0
+        try:
+            return float(value)
+        except ValueError:
+            logging.warning(f"ValueError: {value} is not a valid number")
+
+    def _is_api_position_different(self, ticker: str, api_value: float) -> bool:
+        file_value = self.positions[ticker][1]
+        return api_value != file_value
+
+    def _update_position(self, api_position: dict) -> None:
+        """ update position to file"""
+        ticker = api_position["symbolId"]
+        if ticker not in self.positions.keys():
+            logging.warning(f"Ticker: {ticker} not found in file")
+        else:
+            api_value = self._repr_in_thousands(api_position["convertedValue"])
+            if self._is_api_position_different(ticker=ticker, api_value=api_value):
+                self.sheet.cell(row=self.positions["ticker"][0], col=self.COL_E).value = api_value if api_value else None
+                logging.info(f"UPDATE: {ticker} successful")
+
+    @staticmethod
+    def _get_currencies_converted_value(currencies: List[Dict[str]]) -> float:
+        return round(sum(float(currency["convertedValue"]) for currency in currencies) / 1000, 1)
+
+    def _update_file_exante_available_cash(self, api_currencies: List[Dict[str]]) -> None:
+        api_available_cash = self._get_currencies_converted_value(api_currencies)
+        file_available_cash = self._repr_in_thousands(
+            self.sheet.cell(row=self.available_cash_row, column=self.COL_E).value)
+        if api_available_cash != file_available_cash:
+            self.sheet.cell(row=self.available_cash_row,
+                            column=self.COL_E).value = api_available_cash if api_available_cash else None
+            logging.info(f"UPDATE: {self.AVAILABLE_CASH} successful")
--- src/main.py
+++ src/main.py
@@ -2,52 +2,20 @@ import sys
 
 from openpyxl import load_workbook
 
-from api_services import get_api_data
-from utils import (
-    validate_file_name,
-    update_file_positions_with_api_data,
-    update_file_exante_available_cash, get_currencies_converted_value
-)
-
-# list of accepted files types
-FILES_TYPES = ("xlsx", "xls")
+from src.api_services import get_api_data
+from src.excel import ExelHandler
 
 
 def main():
-    # ensure proper usage: get command line arg or exit
-    if len(sys.argv) != 2:
-        sys.exit("Usage: python extante.py portfolio.xlsx \n"
-                 "Info: README.md")
-
-    file_name = sys.argv[1]
-
-    if not validate_file_name(file_name, FILES_TYPES):
-        sys.exit(f"argument {file_name} is not a valid file type"
-                 f"valid files type: {FILES_TYPES}"
-                 "Info: README.md")
+    file_name = sys.argv[1] if len(sys.argv) == 2 else "portfel.xlsx"
 
     # get API data
     api_data = get_api_data()
 
-    # open file
-    try:
-        file = load_workbook(f'{file_name}')
-    except Exception as e:
-        # FileNotFoundError or LoadError
-        sys.exit(f"Error: {e}")
-    else:
-        sheet = file.active
-
-    # update file
-    api_positions = api_data["positions"]
-
-    available_cash = get_currencies_converted_value(data=api_data["currencies"])
-    update_file_positions_with_api_data(api_positions=api_positions, sheet=sheet)
-    update_file_exante_available_cash(available_cash=available_cash, sheet=sheet)
-
-    # save the file
-    file.save(file_name)
-    print(f"FILE >{file_name}< update successful")
+    file = ExelHandler(file_name=file_name)
+    file.load_file_data()
+    file.update_file_with_api_data(api_data=api_data)
+    file.save_and_close()
 
 
 if __name__ == "__main__":
